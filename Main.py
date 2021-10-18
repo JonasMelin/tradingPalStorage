e6799489a8deb14af1d4f8a7497f95aec1a8a839
Jonas Melin <jonas.melin@bisnode.com>
2021-10-18 20:27:04 +0200

    Fetching and storing stocks transactions to mongo

--- Main.py
+++ Main.py
@@ -8,14 +8,17 @@ import sys
 mongoPort = 27018
 mongoHost = "127.0.0.1"
 databaseName = "TP"
-collectionName = f"tickers"
-URL = "http://127.0.0.1:5000/tradingpal/getAllStocks"
+collectionNameTickers = f"tickers"
+collectionNameTransactions = f"transactions"
+URLTickers = "http://127.0.0.1:5000/tradingpal/getAllStocks"
+URLTransactions = "http://127.0.0.1:5000/tradingpal/getFirstChangeLogItem"
 
 class Main():
     def __init__(self):
-        self.DB, self.COLLECTION, self.MONGO_CLIENT = self._connectDb(
-            mongoHost, mongoPort, databaseName, collectionName)
+        self.DB, self.COLLECTION, self.MONGO_CLIENT = self._connectDb(mongoHost, mongoPort, databaseName, collectionNameTickers)
+        self.DBTrans, self.COLLECTIONTrans, self.MONGO_CLIENT_TRANS = self._connectDb(mongoHost, mongoPort, databaseName, collectionNameTransactions)
         self._testMongoConnection(self.MONGO_CLIENT)
+        self._testMongoConnection(self.MONGO_CLIENT_TRANS)
         self.lastFetchedTickerHash = 0
         self.fixIndex()
 
@@ -36,17 +39,32 @@ class Main():
     def fixIndex(self):
 
         print(f"{datetime.datetime.utcnow()} Creating index")
-        self.DB[collectionName].create_index([('tickerName', ASCENDING)])
-        self.DB[collectionName].create_index([('dateUTC', ASCENDING)])
-        self.DB[collectionName].create_index(name='tickerDate', keys=[('tickerName', ASCENDING), ('dateUTC', ASCENDING)])
+        self.DB[collectionNameTickers].create_index([('tickerName', ASCENDING)])
+        self.DB[collectionNameTickers].create_index([('dateUTC', ASCENDING)])
+        self.DB[collectionNameTickers].create_index(name='tickerDate', keys=[('tickerName', ASCENDING), ('dateUTC', ASCENDING)])
         print(f"{datetime.datetime.utcnow()} Done (creating index)")
+
+    def fetchTransactions(self):
+        try:
+            retData = requests.get(URLTransactions)
+
+            if retData.status_code != 200:
+                print(f"{datetime.datetime.utcnow()} Failed to fetch transactions... ")
+                sys.stdout.flush()
+                time.sleep(60)
+
+            return json.loads(retData.content)
+
+        except Exception as ex:
+            print(f"{datetime.datetime.utcnow()} Failed to fetch transactions: {ex}")
+            return None
 
     def fetchTickers(self):
 
         try:
-            retData = requests.get(URL)
+            retData = requests.get(URLTickers)
             if retData.status_code != 200:
-                print(f"{datetime.datetime.utcnow()} Failed to fetch stocks... retrying")
+                print(f"{datetime.datetime.utcnow()} Failed to fetch stocks... ")
                 sys.stdout.flush()
                 time.sleep(60)
 
@@ -64,7 +82,18 @@ class Main():
             print(f"{datetime.datetime.utcnow()} Failed to fetch tickers: {ex}")
             return None
 
-    def writeToMongo(self, tickerData):
+    def writeTransactionsToMongo(self, transactionsData):
+
+        if transactionsData is None or len(transactionsData) == 0:
+            return
+
+        try:
+            transactionsData['date'] = datetime.datetime.strptime(transactionsData['date'], '%Y-%m-%d %H:%M:%S.%f%z')
+            self.COLLECTIONTrans.insert(transactionsData)
+        except Exception as ex:
+            print(f"{datetime.datetime.utcnow()} Failed to insert transactions to mongo. {ex}")
+
+    def writeTickersToMongo(self, tickerData):
 
         if tickerData is None:
             return
@@ -76,12 +105,12 @@ class Main():
 
             for nextTicker in tickerData['list']:
                 del(nextTicker['currentStock'])
-                nextTicker['dateUTC'] = datetime.datetime.strptime(tickerData['updatedUtc'], '%Y-%m-%d %H:%M:%S.%f')
+                nextTicker['dateUTC'] = datetime.datetime.strptime(tickerData['updatedUtc'], '%Y-%m-%d %H:%M:%S.%f%z')
                 allElementsForMongo.append(nextTicker)
 
             self.COLLECTION.insert_many(allElementsForMongo)
         except Exception as ex:
-            print(f"{datetime.datetime.utcnow()} Failed to insert to mongo. ")
+            print(f"{datetime.datetime.utcnow()} Failed to insert tickers to mongo. {ex}")
 
 
     def mainLoop(self):
@@ -90,8 +119,8 @@ class Main():
         sys.stdout.flush()
 
         while True:
-            tickerData = self.fetchTickers()
-            self.writeToMongo(tickerData)
+            self.writeTickersToMongo(self.fetchTickers())
+            self.writeTransactionsToMongo(self.fetchTransactions())
             sys.stdout.flush()
             time.sleep(10)
 
